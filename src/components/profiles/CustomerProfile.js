80d81308a8b807fe7712fe02d4edbb70a5bfa22c
Elena Spradlin <ekspradlin@gmail.com>
2023-06-30 22:44:07 -0400

    Updated Create Request Page

--- src/components/profiles/CustomerProfile.js
+++ src/components/profiles/CustomerProfile.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
+import "./CustomerProfile.css"
 
 export const CustomerProfiles = () => {
 
@@ -28,6 +29,7 @@ export const CustomerProfiles = () => {
     return <><h2>Your Profile</h2><section className="customer">
         <header className="customer__header"> {user?.fullName} </header>
         <div> Email: {user?.email} </div>
+        <div className="profilePicture">Profile Picture:  <img src={user?.profilePicture} alt="Profile" /> </div>
         <button onClick={() => navigate("/customerprofile/edit")}>Edit Your Information</button>
     </section></>
 }
--- src/components/requests/RequestForm.js
+++ src/components/requests/RequestForm.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
-
+import "./RequestForm.css"
 
 export const RequestForm = () => {
 
@@ -8,12 +8,14 @@ export const RequestForm = () => {
         employeeId: 0,
         customerId: 0,
         serviceId: 0,
-        status: "pending"
+        status: "pending",
+        description: ""
 
     })
 
     const [serviceTypes, setServiceTypes] = useState([])
     const [employees, setEmployees] = useState([])
+    const [serviceDescription, setServiceDescription] = useState("");
 
     const navigate = useNavigate()
 
@@ -27,7 +29,8 @@ export const RequestForm = () => {
             employeeId: parseInt(request.employeeId),
             customerId: parseInt(beetleUserObject.id),
             serviceId: parseInt(request.serviceId),
-            status: "pending"
+            status: "pending",
+            description: request.description
         }
 
         return fetch(`http://localhost:8088/requests`, {
@@ -67,61 +70,103 @@ export const RequestForm = () => {
         []
     );
 
+    useEffect(() => {
+        const selectedService = serviceTypes.find(
+            (service) => service.id === request.serviceId
+        );
+        if (selectedService) {
+            setServiceDescription(selectedService.description);
+        } else {
+            setServiceDescription("");
+        }
+    }, [request.serviceId, serviceTypes]);
+
+
 
     return (
-        <form className="requestForm">
-            <h2 className="requestForm__title">New Request</h2>
-            <fieldset>
-                <div className="form-group">
-                    <label htmlFor="dropdown">Type of Service:</label>
+        <main className="container--requestForm">
+            <section>
+                <h2 className="requestForm__title">New Request</h2>
+                <form className="requestForm">
+                    <fieldset>
+                        <div className="form-group">
+                            <label htmlFor="dropdown"><h3>Select A Service:</h3></label>
 
 
-                    <select id="dropdown"
-                        value={request.serviceId || ""}
+                            <select id="dropdown"
+                                value={request.serviceId || ""}
 
-                        onChange={(event) => {
-                            const copy = { ...request };
-                            copy.serviceId = event.target.value;
-                            update(copy);
-                        }}>
-                        <option disabled value="">Select an option</option>
-                        {serviceTypes.map((request) => (
-                            <option value={request.id} key={request.id}>{request.typeOfService}</option>
-                        )
+                                onChange={(event) => {
+                                    const copy = { ...request };
+                                    copy.serviceId = parseInt(event.target.value);
+                                    update(copy);
+                                }}>
+                                <option disabled value="">Select an option</option>
+                                {serviceTypes.map((service => (
+                                    <option value={service.id} key={service.id}>{service.typeOfService}</option>
+                                )
 
+                                ))}
+                            </select>
+                        </div>
+                    </fieldset>
+                    <div className="serviceInformation">
+                        {serviceDescription && (
+                            <div className="form--description">
+                                <h3>Description:</h3>
+
+                                <h4>{serviceDescription}</h4>
+
+                            </div>
                         )}
-                    </select>
-                </div>
-            </fieldset>
+                    </div>
 
-            <fieldset>
-                <div className="form-group">
-                    <label htmlFor="dropdown">Select who you'd like perform this service:</label>
+                    <label htmlFor="description"><h3>Describe your issue:</h3></label>
+                    <input
+                        required autoFocus
+                        type="text"
+                        className="form-control"
+                        placeholder="Brief description of problem"
+                        value={request.description}
+                        onChange={
+                            (evt) => {
+                                const copy = { ...request }
+                                copy.description = evt.target.value
+                                update(copy)
+
+                            }
+                        } />
+                    <fieldset>
+                        <div className="form-group">
+                            <label htmlFor="dropdown"><h3>Select who you'd like perform this service:</h3></label>
 
 
-                    <select id="dropdown"
-                        value={request.employeeId || ""}
+                            <select id="dropdown"
+                                value={request.employeeId || ""}
 
-                        onChange={(event) => {
-                            const copy = { ...request };
-                            copy.employeeId = (event.target.value);
-                            update(copy);
-                        }}>
-                        <option disabled value="">Select an employee</option>
+                                onChange={(event) => {
+                                    const copy = { ...request };
+                                    copy.employeeId = (event.target.value);
+                                    update(copy);
+                                }}>
+                                <option disabled value="">Select an employee</option>
 
-                        {employees.map((request) => (
-                            <option value={request.id} key={request.id}>{request.user?.fullName}</option>
-                        )
+                                {employees.map((request) => (
+                                    <option value={request.id} key={request.id}>{request.user?.fullName}</option>
+                                )
 
-                        )}
-                    </select>
-                </div>
-            </fieldset>
+                                )}
+                            </select>
+                        </div>
+                    </fieldset>
 
-            <button
-                onClick={(clickEvent) => handleSubmitButtonClick(clickEvent)}
-                className="btn btn-primary">
-                Submit Request
-            </button>
-        </form >)
+                    <button
+                        onClick={(clickEvent) => handleSubmitButtonClick(clickEvent)}
+                        className="btn btn-primary">
+                        Submit Request
+                    </button>
+                </form>
+            </section>
+        </main >
+    )
 }
